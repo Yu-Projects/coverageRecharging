15f725e800bebd3199cbe3524d723c9601fc3363
Kevin Li Yu <klyu@vt.edu>
2018-06-01 17:14:16 -0400

    added new file makingGroupedPoints.m/finished Type1 edges/need to create all other types of edges

--- testGeneral.m
+++ testGeneral.m
@@ -33,7 +33,10 @@ h = scatter3(x3d, y3d, z3d);
 [v_Cluster] = makingV_cluster(numPointsInit, numBatteryLevels);
 v_Cluster = num2cell(v_Cluster);
 
-[v_Adj, v_Type, S1, T1, weights] = makingSTWv_AdjGeneral(maxDistance, x1, y1, numPointsInit, numBatteryLevels, v_Cluster, timeTO, timeL, rechargeRate, UGVSpeed);
+[groupedPoints] = makingGroupedPoints(numPointsInit, numBatteryLevels);
+groupedPoints = num2cell(groupedPoints);
+
+[v_Adj, v_Type, S1, T1, weights] = makingSTWv_AdjGeneral(maxDistance, x1, y1, numPointsInit, numBatteryLevels, v_Cluster, timeTO, timeL, rechargeRate, UGVSpeed, groupedPoints);
 
 [xOut, yOut] = graphingCluster(x1, y1, numPointsInit, numBatteryLevels, S1, T1, 0, nodeArray, method);            % graph in cluster format
 
